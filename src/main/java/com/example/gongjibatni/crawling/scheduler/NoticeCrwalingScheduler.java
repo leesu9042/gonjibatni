bc4f7dfc6d2d8e3bfeb86306394bbebddf6b0848
leesu9042 <tnckddl8957@gmail.com>
2026-02-12 21:41:42 +0900

    feat: 크롤링 스케줄링 기능 구현

--- src/main/java/com/example/gongjibatni/crawling/scheduler/NoticeCrwalingScheduler.java
+++ src/main/java/com/example/gongjibatni/crawling/scheduler/NoticeCrwalingScheduler.java
@@ -1,29 +1,34 @@
-//package com.example.gongjibatni.crawling;
-//
-//
-//
-//import com.example.gongjibatni.crawling.service.CrawlingService;
-//import org.springframework.scheduling.annotation.Scheduled;
-//import org.springframework.stereotype.Component;
-//
-//import static com.example.gongjibatni.NoticeUrl.ACADEMIC;
-//
-//@Component
-//public class NoticeCrwalingScheduler {
-//
-//    private final CrawlingService crawlingService;
-//
-//    public NoticeCrwalingScheduler(CrawlingService crawlingService) {
-//        this.crawlingService = crawlingService;
-//    }
-//
-//
-//
-//    @Scheduled(fixedRateString  = "${crawl.rate}") //3시간
-//    public void runCrawlJob(){
-//        crawlingService.crawlAndSave(ACADEMIC.getUrl());
-//
-//
-//
-//    }
-//}
+package com.example.gongjibatni.crawling.scheduler;
+
+
+
+import com.example.gongjibatni.crawling.service.CrawlingService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import static com.example.gongjibatni.NoticeUrl.ACADEMIC;
+
+@Component
+public class NoticeCrwalingScheduler {
+
+    private final CrawlingService crawlingService;
+    private static final Log log = LogFactory.getLog(NoticeCrwalingScheduler.class);
+
+
+    public NoticeCrwalingScheduler(CrawlingService crawlingService) {
+        this.crawlingService = crawlingService;
+    }
+
+
+
+    @Scheduled(fixedRateString = "${crawl.rate:10800000}") //3시간
+    public void runCrawlJob(){
+
+        log.info("[CrawlScheduler] academic notice crawl started");
+        crawlingService.crawlLatestNotices(ACADEMIC.getUrl());
+
+
+    }
+}
